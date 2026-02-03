0dc7b844b47a4291ab2f05f9e87767613a67d582
Anslie Pettit <apettit1112@gmail.com>
2026-02-03 09:40:25 -0800

    Step 10

--- tests/textAnalyzer.test.js
+++ tests/textAnalyzer.test.js
@@ -1,15 +1,33 @@
 const { countWords, findLongestWord, countLines } = require('../src/textAnalyzer');
 
-const path = 'data/quotes.txt';
+describe('quotes.txt analysis', () => {
+  const path = 'data/quotes.txt';
 
-test('counts words in quotes.txt', () => {
-  expect(countWords(path)).toBe(108);
+  test('counts words in quotes.txt', () => {
+    expect(countWords(path)).toBe(108);
+  });
+
+  test('finds longest word in quotes.txt', () => {
+    expect(findLongestWord(path)).toBe('sophistication');
+  });
+
+  test('counts lines in quotes.txt', () => {
+    expect(countLines(path)).toBe(10);
+  });
 });
 
-test('finds longest word in quotes.txt', () => {
-  expect(findLongestWord(path)).toBe('sophistication');
-});
+describe('sample-text.txt analysis', () => {
+  const path = 'data/sample-text.txt';
 
-test('counts lines in quotes.txt', () => {
-  expect(countLines(path)).toBe(10);
+  test('counts words in sample-text.txt', () => {
+    expect(countWords(path)).toBe(207);
+  });
+
+  test('finds longest word in sample-text.txt', () => {
+    expect(findLongestWord(path)).toBe('infrastructure');
+  });
+
+  test('counts lines in sample-text.txt', () => {
+    expect(countLines(path)).toBe(1);
+  });
 });
